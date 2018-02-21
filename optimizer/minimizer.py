b5751231bb1d32fb301a72e092596adf2af5828e
Cristina E. González-Espinoza <crisbeth46@gmail.com>
2018-02-21 17:16:34 -0500

    Added things to make the module more formal.

--- optimizer/minimizer.py
+++ optimizer/minimizer.py
@@ -1,3 +1,22 @@
+# Optimizer of Long-Range model Hamiltonians
+# Copyright (C) 2018 Cristina E. Gonzalez-Espinoza
+# <gonzalce@mcmaster.ca>.
+# 
+# This file is part of LROptimizer.
+# 
+# LROptimizer is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+# 
+# LROptimizer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>
+
 """Variational optimizer of long-range erfgau potential"""
 
 import numpy as np
